ac3cd3cd195a045b7b249cfd709065e3b5d12375
manishreddy200 <177r1a04e3@gmail.com>
2022-07-08 23:20:38 +0530

    added responsiveness

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses.component';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { SharedModule } from '../../shared/shared.module';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { CourseListComponent } from './course-list/course-list.component';
 
-
-
 @NgModule({
-  declarations: [
-    CoursesComponent,
-    CourseCardComponent,
-    CourseListComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-  ],
-  exports: [CoursesComponent]
+  declarations: [CoursesComponent, CourseCardComponent, CourseListComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [CoursesComponent],
 })
-export class CoursesModule { }
+export class CoursesModule {}
